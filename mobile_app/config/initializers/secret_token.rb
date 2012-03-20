b4f0e677d2a3ce473d42699b860d9a53074f1fb3
tomslinnesexitclear;backquit <toms.linnes@gmal.com>
2012-03-19 22:32:23 -0700

    Created the rails app

---
+++ mobile_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileApp::Application.config.secret_token = '7587f6ded475c74524fffb6cd123987d4fcbbc98099bd0135302f6b92bed86e35613f245266570c4c394b339fc235be02dba790153931c9457704d55b0ded718'
